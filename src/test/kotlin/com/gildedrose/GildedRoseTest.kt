c86fcc3268dbf2a1c1f49a97f9c58aa7bcb3e5e4
tarannum-2002 <tarannums@sahaj.ai>
2024-02-25 16:44:36 +0530

    modify test for name

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,13 +6,19 @@ import org.junit.jupiter.api.Test
 internal class GildedRoseTest {
 
     @Test
-    fun foo() {
+    fun `check if the class updates the name as given`() {
         val items = listOf(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+        assertEquals("foo", app.items[0].name)
 
     }
+
+    
+
+
+
+
 
 }
 
